92a77f58b90d029553e48b15d511c40156b2b897
hengyuxin <hengyuxin@qq.com>
2024-01-13 20:25:27 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="page-container">
+		<u-swiper
+			class="page-banber"
+			:list="bannerList"
+			height="130"
+		></u-swiper>
+		<u-button text="月落"></u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				bannerList: [
+					'https://img1.baidu.com/it/u=25340056,1897490112&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800',
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.page-banber {
+		width: 100%;
+	}
+</style>
